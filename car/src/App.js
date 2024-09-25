992e81d7511ea29f1a45d85c5d2515518c5e99b5
DESKTOP-4K7N2Q0\user <wnstjr4785@gmail.com>
2024-09-25 12:00:44 +0900

    화면 업데이트

--- car/src/App.js
+++ car/src/App.js
@@ -2,6 +2,9 @@ import './CSS/App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Signup from './Signup';
 import Admin from './Admin';
+import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom";
+import axios from 'axios';
 
 function App() {
   return (
@@ -18,6 +21,31 @@ function App() {
 }
 
 function Login() {
+  const [emailOrPhone, setEmailOrPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const registerNavigate = async (e) => {
+    navigate("./Signup",  { replace: false})
+  };
+  const handleLogin = async (e) => {
+      e.preventDefault(); // 기본 폼 제출 이벤트 방지
+      setError(''); // 이전 에러 메시지 초기화
+
+      try {
+          const response = await axios.post('http://127.0.0.1:8000/api/login/', {
+              email_or_phone: emailOrPhone,
+              password: password,
+          });
+          // 로그인 성공 시 처리 (예: 토큰 저장, 리다이렉트 등)
+          console.log('로그인 성공:', response.data);
+      } catch (err) {
+          console.error('로그인 실패:', err.response?.data);
+          setError('로그인에 실패했습니다. 이메일 또는 비밀번호를 확인하세요.');
+      }
+  };
+
   return (
     <div className="login-container">
       <form className="login-form">
@@ -28,11 +56,13 @@ function Login() {
           <input type="password" id="password" name="password" placeholder="비밀번호" />
         </div>
         <div className="form-footer">
-          <Link to="/signup" className="signup-label">회원가입</Link>
-          <div className="auto-login">
+        <div className="auto-login">
             <input type="checkbox" id="autoLogin" name="autoLogin" />
-            <label htmlFor="autoLogin">자동로그인</label>
+            <label onClick={handleLogin} htmlFor="autoLogin">자동로그인</label>
           </div>
+          <Link to="/signup" className="signup-label">회원가입</Link>
+          
+          <button className="login-button">로그인</button>
         </div>
       </form>
     </div>
