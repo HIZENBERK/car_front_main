768d1c9ecabcadb6fed177eaa748a2c95cb72343
DESKTOP-4K7N2Q0\user <wnstjr4785@gmail.com>
2024-09-27 13:14:53 +0900

    사이드바에 설정 추가와 그에 따른 디자인 수정

--- car/src/View/Sidebar.js
+++ car/src/View/Sidebar.js
@@ -9,6 +9,7 @@ const Sidebar = () => {
     };
 
     const menuItems = ['대시보드', '사용자 관리', '차량 관리', '차량 운행 내역', '지출관리']; // 메뉴 항목들
+    const settingsMenuItems = ['설정']; // 설정 메뉴 항목들
 
     return (
         <div className="sidebar">
@@ -23,6 +24,21 @@ const Sidebar = () => {
                         <div
                             className={`menu-item ${selectedItem === index ? 'selected' : ''}`} // 선택된 항목에 'selected' 클래스 추가
                             onClick={() => handleClick(index)} // 클릭 시 해당 인덱스를 선택
+                        >
+                            {item}
+                        </div>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+            <div className="settings-menu">
+                <hr />
+                
+                {settingsMenuItems.map((item, index) => (
+                    <div key={index}>
+                        <div
+                            className={`menu-item ${selectedItem === menuItems.length + index ? 'selected' : ''}`} // 선택된 항목에 'selected' 클래스 추가
+                            onClick={() => handleClick(menuItems.length + index)} // 클릭 시 해당 인덱스를 선택
                         >
                             {item}
                         </div>
