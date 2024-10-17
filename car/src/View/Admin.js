b549a1d0fd1bd6d678e8de666719611aaf8e1e28
CHOI\dbrrn <xmrwjstk1112@naver.com>
2024-10-17 17:53:49 +0900

    Admin 페이지 회사 사용자수 조회 가능 그리고 사용자 회원가입 만들고 있는데 아직 미완성

--- car/src/View/Admin.js
+++ car/src/View/Admin.js
@@ -1,100 +1,107 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/Admin.css';
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement, // 만약 바 차트를 사용하는 경우
-  ArcElement // 만약 파이 차트를 사용하는 경우
+  BarElement, 
+  ArcElement 
 } from 'chart.js';
-import PieChart from './PieChart';  // PieChart 가져오기
+import PieChart from './PieChart';  
 import BarChart from './BarChart';
-import {useAuth} from "../Component/AuthContext";  // BarChart 가져오기
+import { useAuth } from "../Component/AuthContext";
+import axios from 'axios';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement, // 바 차트일 경우
-  ArcElement // 파이 차트일 경우
+  BarElement, 
+  ArcElement 
 );
 
 function Admin() {
-  const handleClick = (item) => {
-    console.log(`${item} 클릭됨`); // 클릭 이벤트 핸들러 (나중에 실제 로직으로 대체 가능)
+  const { authState } = useAuth(); // useAuth 훅에서 authState 가져오기
+  const [userCount, setUserCount] = useState(0);
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const navigate = useNavigate();  // 경로 이동을 위한 useNavigate 훅
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axios.get('https://hizenberk.pythonanywhere.com/api/users/', {
+        headers: {
+          Authorization: `Bearer ${authState.access}` 
+        },
+      });
+
+      // is_admin이 false인 유저만 필터링하여 사용자 수 저장
+      const nonAdmins = response.data.users.filter((user) => !user.is_admin);
+      setUserCount(nonAdmins.length);
+    } catch (err) {
+      console.error('조회 실패:', err.response?.data);
+    }
   };
 
-  const [selectedMonth, setSelectedMonth] = useState('');
+  useEffect(() => {
+    // 사용자가 로그인 상태일 때 데이터를 가져오기
+    if (authState?.access) {
+      getUserInfo();
+    }
+  }, [authState]);
 
   const handleYearChange = (event) => {
     const selectedYear = event.target.value;
-    // 선택된 년도에 따른 데이터를 불러오는 로직
-    console.log(`선택된 년도: ${selectedYear}`);
-    
-    // API 호출 또는 로컬 데이터를 가져오는 부분
     fetchDataForYear(selectedYear);
   };
-  
+
   const fetchDataForYear = (year) => {
-    // 선택된 년도에 맞는 데이터를 불러오는 로직 구현
-    // 예시: API 호출 또는 상태 업데이트
     console.log(`${year}년 데이터를 불러옵니다.`);
   };
 
-  //Month Select
   const handleMonthChange = (event) => {
     const selectedMonth = event.target.value;
-    // 선택된 달에 따른 데이터를 불러오는 로직
-    console.log(`선택된 달: ${selectedMonth}`);
-    setSelectedMonth(selectedMonth); // 선택된 달을 상태로 업데이트
+    setSelectedMonth(selectedMonth);  // 선택된 달을 상태로 업데이트
     fetchDataForMonth(selectedMonth);
-    
-    // API 호출 또는 로컬 데이터를 가져오는 부분
-    fetchDataForMonth(selectedMonth);
-  };
-  const { authState } = useAuth();
-  
-  const fetchDataForMonth = (Month) => {
-    // 선택된 달에 맞는 데이터를 불러오는 로직 구현
-    // 예시: API 호출 또는 상태 업데이트
-    console.log(`${Month}월 데이터를 불러옵니다.`);
   };
 
-   // 공지사항 데이터를 배열로 정의 (제목과 작성 날짜)
+  const fetchDataForMonth = (month) => {
+    console.log(`${month}월 데이터를 불러옵니다.`);
+  };
+
+  // 공지사항 데이터를 배열로 정의 (제목과 작성 날짜)
   const notices = [
     { title: '공지사항 1', date: '2024-09-30' },
     { title: '공지사항 2', date: '2024-10-01' },
     { title: '공지사항 3', date: '2024-10-02' },
   ];
-  const navigate = useNavigate(); // useNavigate 훅 호출
 
-  // 클릭 핸들러 함수
+  // 공지사항 클릭 핸들러 함수
   const handleNoticeClick = (notice) => {
-      console.log(`${notice.title} 클릭됨`);
-      navigate(`/notice/${notice.title}`); // 해당 공지 제목에 맞는 경로로 이동
+    console.log(`${notice.title} 클릭됨`);
+    navigate(`/notice/${notice.title}`);  // 공지사항 제목에 맞는 경로로 이동
   };
+
+  // authState가 아직 로딩 중일 때 로딩 메시지 표시
+  if (!authState) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="admin-container">
-      
       <div className="admin-top">
         <p className="office-name">회사명 : {authState.company_name}</p>
-        <p className="person-name"> {authState.name} 님 안녕하세요!</p>
+        <p className="person-name">{authState.name} 님 안녕하세요!</p>
       </div>
 
       <div className="admin-a-box">
-
         <div className="admin-b-box">
-
           <div className="admin-d-box">
-
             <div className="admin-f-box">
-
               <div className="admin-middle">
                 <div className="user-info-box">
                   <p className="admin-middle-title">사용자</p>
                   <div className="admin-middle-content">
-                    <p className="admin-middle-text">전체</p>
+                    <p className="admin-middle-text">전체: {userCount}명</p>
                     <p className="admin-middle-text">신규</p>
                     <p className="admin-middle-text">사용중지</p>
                   </div>
@@ -117,20 +124,16 @@ function Admin() {
                   </div>
                 </div>
               </div>
-
             </div>
 
             <div className="admin-g-box">
-
               <div className="user-guide-box">
                 <p className="admin-middle-title">사용자 가이드</p>
                 <div className="user-guide-content">
                   {/* 여기에 사용자 가이드 내용이 들어갑니다 */}
                 </div>
               </div>
-
             </div>
-
           </div>
 
           <div className="admin-e-box">
@@ -140,31 +143,27 @@ function Admin() {
                 <table>
                   <thead>
                     <tr>
-                        <th>제목</th>
-                        <th className="notice-date-th">작성 날짜</th>
+                      <th>제목</th>
+                      <th className="notice-date-th">작성 날짜</th>
                     </tr>
                   </thead>
                   <tbody>
                     {notices.map((notice, index) => (
-                        <tr key={index} onClick={() => handleNoticeClick(notice)} className="clickable-row">
-                            <td>{notice.title}</td>
-                            <td className="notice-date-td">{notice.date}</td>
-                        </tr>
+                      <tr key={index} onClick={() => handleNoticeClick(notice)} className="clickable-row">
+                        <td>{notice.title}</td>
+                        <td className="notice-date-td">{notice.date}</td>
+                      </tr>
                     ))}
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
-          
         </div>
 
         <div className="admin-c-box">
-
           <div className="admin-bottom-a-box">
-
             <div className="admin-bottom-b-box">
-
               <div className="admin-bottom-c-box">
                 <p className="admin-bottom-title">운행 현황</p>
                 <select className="year-select" onChange={handleYearChange}>
@@ -190,74 +189,6 @@ function Admin() {
               </div>
 
               <div className="admin-bottom-d-box">
-                
-                <div className="admin-bottom-e-box">
-                  <p className="operation-status-middle-title">운행건수</p>
-                  <div className="op-box">
-                     <p className="operation-number-count">1,000</p>
-                     <p className="op-count"> 건</p>
-                  </div>
-                </div>
-                <div className="user-average-box">
-                  <div className="daily-average-box">
-                    <p className="daily-average">일 평균</p>
-                    <p className="daily-average-text">30</p>
-                  </div>
-                  <div className="weekly-average-box">
-                    <p className="weekly-average">주 평균</p>
-                    <p className="weekly-average-text">60</p>
-                  </div>
-                  <div className="monthly-average-box">
-                    <p className="monthly-average">월 평균</p>
-                    <p className="monthly-average-text">280</p>
-                  </div>
-                </div>
-
-              </div>
-
-            </div>
-
-            <div className="admin-bottom-b-box">
-
-              <div className="admin-bottom-c-box">
-
-              </div>
-
-              <div className="admin-bottom-d-box">
-
-                <div className="admin-bottom-e-box">
-                  <p className="operation-status-middle-title">운행거리</p>
-                  <div className="op-box">
-                    <p className="operation-number-count">1,000</p>
-                    <p className="op-count"> km</p>
-                  </div>
-                </div>
-                <div className="user-average-box">
-                  <div className="daily-average-box">
-                    <p className="daily-average">일 평균</p>
-                    <p className="daily-average-text">30</p>
-                  </div>
-                  <div className="weekly-average-box">
-                    <p className="weekly-average">주 평균</p>
-                    <p className="weekly-average-text">60</p>
-                  </div>
-                  <div className="monthly-average-box">
-                    <p className="monthly-average">월 평균</p>
-                    <p className="monthly-average-text">280</p>
-                  </div>
-                </div>
-                
-              </div>
-
-            </div>
-
-            <div className="admin-bottom-b-box">
-
-              <div className="admin-bottom-c-box">
-               <p className="operation-status-top-title">{selectedMonth ? `${selectedMonth}월 운행비율` : '1월 운행비율'}</p>
-              </div>
-
-              <div className="admin-bottom-d-box">
                 <div className="operation-percentage">
                   <div className="a-label-box">
                     <div className="a-label" />
@@ -270,14 +201,11 @@ function Admin() {
                   <PieChart />
                 </div>
               </div>
-
             </div>
 
             <div className="admin-bottom-b-box">
-
               <div className="admin-bottom-c-box">
                 <p className="month-distance-title">월간 운행비율</p>
-                
               </div>
 
               <div className="admin-bottom-d-box">
@@ -293,17 +221,10 @@ function Admin() {
                   <BarChart />
                 </div>
               </div>
-
             </div>
-
           </div>
-          
         </div>
-
-      </div> 
-
-      
-
+      </div>
     </div>
   );
 }
