5f44f2b8663c4310a7629733d056899c1280536f
HIZENBERK <135631332+HIZENBERK@users.noreply.github.com>
2024-10-28 11:41:44 +0900

    지도 기능 업데이트

--- car/src/Component/UseMap.js
+++ car/src/Component/UseMap.js
@@ -1,32 +1,42 @@
-//지도 사용 api
-import { useEffect, useState } from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
 
 const { Tmapv2 } = window;
 
-export const useMap = (mapRef: React.RefObject<HTMLDivElement>) => {
-    //mapRef: React.RefObject<HTMLDivElement>
-    // const [mapInstance, setMapInstance] = useState<TMap | null>(null);
-    //
-    // useEffect(() => {
-    //     if (mapRef.current?.firstChild || mapInstance) {
-    //         return;
-    //     }
-    //
-    //     const map = new Tmapv2.Map('map', {
-    //         zoom: DEFAULT_ZOOM_LEVEL,
-    //         zoomControl: false,
-    //         center: new Tmapv2.LatLng(INITIAL_LATITUDE, INITIAL_LONGITUDE),
-    //     });
-    //
-    //     map.setZoomLimit(MIN_ZOOM_LEVEL, MAX_ZOOM_LEVEL);
-    //     setMapInstance(map);
-    // }, [mapRef, mapInstance]);
-    const map = new window.Tmapv2.Map("map_div",
-        {
-            center: new Tmapv2.LatLng(37.566481622437934,126.98502302169841), // 지도 초기 좌표
-            width: "890px",
-            height: "400px",
-            zoom: 15
-        });
+const UseMap = ({ onClose , isOpen }) => {
+    const mapRef = useRef(null); // 지도를 그릴 div에 대한 ref
+    const [map, setMap] = useState(null); // Tmap 인스턴스 저장
+    const initTmap = () => {
+        if (mapRef.current && !map) {
+            console.log("맵 객체 초기화 진행");
+            const mapInstance = new Tmapv2.Map(mapRef.current, {
+                center: new Tmapv2.LatLng(37.566481622437934, 126.98502302169841),
+                width: "100%",
+                height: "400px",
+                zoom: 15,
+            });
+            setMap(mapInstance);
+        }
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            initTmap();  // 모달이 열릴 때만 초기화
+        } else if (map) {
+            console.log("맵 객체 해제");
+            mapRef.current.destroy();  // 기존 맵 인스턴스 해제
+            mapRef.current = null;
+            setMap(null);
+        }
+    }, [isOpen]);
+
+    return (
+        <div className="map-container" id="map-container">
+            <div ref={mapRef} style={{ width: '80%', height: '400px' }} />
+            <button onClick={onClose} className="close-button">
+                닫기
+            </button>
+        </div>
+    );
 };
+
+export default UseMap;
