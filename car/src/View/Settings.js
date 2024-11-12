a63bb70eea0e98e91d178aa53111609a2b377732
HIZENBERK <135631332+HIZENBERK@users.noreply.github.com>
2024-11-12 16:25:50 +0900

    출처 표시 토글(탭)추가

--- car/src/View/Settings.js
+++ car/src/View/Settings.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../CSS/Settings.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Settings() {
+    const [showSources, setShowSources] = useState(false);
+
     const menuItems = [
         { name: '대시보드', icon: 'fas fa-tachometer-alt', color: 'black', source: 'https://fontawesome.com/icons' },
         { name: '공지사항', icon: 'fas fa-bell', color: 'black', source: 'https://fontawesome.com/icons' },
@@ -10,25 +12,36 @@ function Settings() {
         { name: '차량 관리', icon: 'fas fa-car', color: 'black', source: 'https://fontawesome.com/icons' },
         { name: '차량 운행 내역', icon: 'fas fa-history', color: 'black', source: 'https://fontawesome.com/icons' },
         { name: '지출관리', icon: 'fas fa-money-bill', color: 'black', source: 'https://fontawesome.com/icons' },
-        { name: '뒤로 가기', icon: 'bi bi-arrow-return-left', color: 'black', source: 'https://icons.getbootstrap.kr/icons/arrow-return-left/'}
+        { name: '뒤로 가기', icon: 'bi bi-arrow-return-left', color: 'black', source: 'https://icons.getbootstrap.kr/icons/arrow-return-left/' }
     ];
+
+    const toggleSources = () => {
+        setShowSources(!showSources);
+    };
 
     return (
         <div className="setting-container">
-            {menuItems.map((item, index) => (
-                <div key={index} className="setting-menu-item">
-                    <i className={`${item.icon} setting-icon`}></i>
-                    <span className="setting-menu-name">{item.name}</span>
-                    <span className="setting-source">출처: {item.source}</span>
-                </div>
-            ))}
-            <div className="setting-company-section">
-                <div className="setting-profile-circle">
-                    <img src={require('../Img/car.jpg')} alt="Car"/>
-                </div>
-                <span className="setting-menu-name">자동차 이미지</span>
-                <span className="setting-source">출처: https://kor.pngtree.com/so/%EC%9E%90%EB%8F%99%EC%B0%A8-%EC%95%84%EC%9D%B4%EC%BD%98 </span>
+            <div className="setting-source-toggle" onClick={toggleSources}>
+                아이콘 출처 {showSources ? '▲' : '▼'}
             </div>
+            {showSources && (
+                <div className="setting-source-list">
+                    {menuItems.map((item, index) => (
+                        <div key={index} className="setting-menu-item">
+                            <i className={`${item.icon} setting-icon`}></i>
+                            <span className="setting-menu-name">{item.name}</span>
+                            <span className="setting-source">출처: {item.source}</span>
+                        </div>
+                    ))}
+                    <div className="setting-company-section">
+                        <div className="setting-profile-circle">
+                            <img src={require('../Img/car.jpg')} alt="Car"/>
+                        </div>
+                        <span className="setting-menu-name">자동차 이미지</span>
+                        <span className="setting-source">출처: https://kor.pngtree.com/so/%EC%9E%90%EB%8F%99%EC%B0%A8-%EC%95%84%EC%9D%B4%EC%BD%98 </span>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
